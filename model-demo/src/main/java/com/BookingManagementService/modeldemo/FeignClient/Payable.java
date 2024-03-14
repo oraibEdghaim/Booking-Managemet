7f049ca5615d9d058c7bfed8c4212ff857efc064
Yusra <y.kanakri310@gmail.com>
2024-03-14 17:37:45 +0300

    Eureka needed changes

--- model-demo/src/main/java/com/BookingManagementService/modeldemo/FeignClient/Payable.java
+++ model-demo/src/main/java/com/BookingManagementService/modeldemo/FeignClient/Payable.java
@@ -4,10 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "PaymentFeignClient",url = "http://PAYMENT-SYSTEM/Payment")
+@FeignClient(name="Payment-System")
+        //(name = "PaymentFeignClient",url = "http://localhost:9090/Payment")
 public interface Payable {
 
-    @PostMapping("/pay")
+    @PostMapping("/Payment/pay")
     public String pay(@RequestParam String serviceName, @RequestParam double price, @RequestParam String currency);
 
 }
--- model-demo/src/main/java/com/BookingManagementService/modeldemo/ModelDemoApplication.java
+++ model-demo/src/main/java/com/BookingManagementService/modeldemo/ModelDemoApplication.java
@@ -7,10 +7,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+//@Configuration
 @EnableDiscoveryClient
 @OpenAPIDefinition(
 		info = @Info(title = "Book Management Service",version ="1.0.1" ),
@@ -24,5 +29,11 @@ public class ModelDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ModelDemoApplication.class, args);
 	}
+
+//	@Bean
+//	@LoadBalanced
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
 
 }
